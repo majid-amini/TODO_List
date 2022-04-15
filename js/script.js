f879a6a8a4e27564c5884f3d478d747042606b98
majid Amini <majidamini_s@yahoo.com>
2022-04-15 13:57:57 +0430

    some bugs for local storage fixed

--- js/script.js
+++ js/script.js
@@ -1,51 +1,93 @@
 let $ = document;
 const inputTask = $.querySelector('#input_task');
 const addTaskBtn = $.querySelector('.add_task');
-const ulTag = $.querySelector('.tasks_ul');
+const ulElem = $.querySelector('.tasks_ul');
 const deleteBtn = $.querySelector('.delete');
+const addTodoForm = $.querySelector('.new_task_total');
+const clearContainer = $.querySelector('.clear_tasks')
+const clearBtn = $.querySelector('#clear');
+
+addTodoForm.addEventListener('submit',function(event){
+    event.preventDefault();
+})
 
 let todosArray = [];
 
+inputTask.addEventListener('keydown',function(event){
 
-function addTaskFunc(event) {
-    event.preventDefault();
-    let newTodoTitle = inputTask.value;
+    let newTodoValue = event.target.value.trim();
+    
+    if (event.keyCode === 13) {
+        if (newTodoValue) {
+           addNewTodo(newTodoValue);
+           
+        }
+    }
+});
+
+
+
+function addNewTodo(newTodoValue){
+
+
     let newTodoObj = {
-        id: todosArray.length + 1,
-        title: newTodoTitle,
+        id: todosArray.length +1,
+        title: newTodoValue
     }
     
-    inputTask.value = '';
-
     todosArray.push(newTodoObj);
-    setLocalStorage(todosArray);
+    setlocalStorage(todosArray);
     todosCreator(todosArray);
     
+    
+    let newTodoLi = $.createElement('li');
+    newTodoLi.className = "tasks_item";
+
+    inputTask.value = '';
+
+    let newTodoTitleSpan = $.createElement('span');
+    newTodoTitleSpan.className = "title-span";
+    newTodoTitleSpan.innerHTML = newTodoValue;
+    
+    let newTodoDeleteIcon = $.createElement('i');
+    newTodoDeleteIcon.className = "fa-solid fa-trash-can delete";
+
+    newTodoDeleteIcon.addEventListener('click',function(event){
+        event.target.parentElement.remove();
+    })
+
+   newTodoLi.append(newTodoTitleSpan, newTodoDeleteIcon);
+
+    
+    ulElem.append(newTodoLi);
 }
 
-function setLocalStorage(todosList) {
+
+function setlocalStorage(todosList) {
     localStorage.setItem('todos',JSON.stringify(todosList));
 }
 
-
 function todosCreator(todosList) {
-    todosList.forEach(function(item) {
-        
-        let newTodoLiElem = $.createElement('li');
-        newTodoLiElem.className = "tasks_item";
-
-
-        let spanElem = $.createElement('span');
-        spanElem.innerHTML = item.title;
-        
-
-        let newTodoDeleteIcon = $.createElement('i');
-        newTodoDeleteIcon.className = "fa-solid fa-trash-can delete";
-
-        newTodoLiElem.append(spanElem, newTodoDeleteIcon);
-        ulTag.append(newTodoLiElem);
-        
+    todosList.forEach(function (todo){
+        console.log(todo);
     })
 }
 
-addTaskBtn.addEventListener('click',addTaskFunc);
+
+
+
+
+
+
+
+
+// clearBtn.addEventListener('click',function(event){
+//     console.log(event.target.parentElement.parentElement.children)
+// })
+
+// function taskNumbers(newTodoLi) {
+//     let taskInfo = document.createElement('p');
+    
+
+//     taskInfo.innerHTML = `you have ${} pending tasks`
+// }
