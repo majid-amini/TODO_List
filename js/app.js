5389f50570226b0ef4c12283651be96073ab911c
majid Amini <majidamini_s@yahoo.com>
2022-04-15 23:14:45 +0430

    focus function and some styles for cleartask section added

--- js/app.js
+++ js/app.js
@@ -3,38 +3,22 @@ const inputTask = $.querySelector('#input_task');
 const addBtn = $.querySelector('.add_task');
 const ulElement = $.querySelector('.tasks_ul');
 const clearBtn = $.querySelector('#clear');
+const clearTaskDiv = $.querySelector('.clear_tasks');
 const formElem = $.querySelector('.new_task_total');
 
 let todosArray = [];
 
-// formElem.addEventListener('submit',function(event){
-//     event.preventDefault();
-// })
-
-addBtn.addEventListener('click',function(event){
+formElem.addEventListener('submit',function(event){
     event.preventDefault();
-
-    let inputTaskValue = inputTask.value;
-
-    let newTodoObj = {
-        id: todosArray.length +1,
-        title: inputTaskValue
-    }
-
-    inputTask.value = '';
-
-    todosArray.push(newTodoObj);
-    setLocalStorage(todosArray);
-    todosGenerator(todosArray);
-    tasksCounter();
-    
 })
 
 
 
+addBtn.addEventListener('click',addTask);
+   
+
 function setLocalStorage(todosList){
     localStorage.setItem('todos',JSON.stringify(todosList));
-    
 }
 
 function todosGenerator(todosList) {
@@ -90,20 +74,58 @@ function removeItemlocalStorage(id) {
 }
 
 function tasksCounter() {
-    $.querySelector('.clear_tasks span').innerHTML = todosArray.length; 
+   let liCounter =  $.querySelector('.clear_tasks span');
+   liCounter.innerHTML = todosArray.length; 
+   liCounter.style.color = "red";
+   liCounter.style.fontWeight = "bold";
+   liCounter.style.fontSize = "1.5rem";
+   liCounter.style.textShadow = "0.1rem 0.2rem 0.2rem #000";
+   liCounter.style.backgroundColor = "#ccc";
+   liCounter.style.borderRadius = "50%";
+   liCounter.style.padding = "1rem";
+   
+   if (liCounter.innerHTML == 0) {
+       clearTaskDiv.style.display = 'none';
+   } else {
+       clearTaskDiv.style.display = 'flex';
+   }
+
 }
 
 
 function clearTodos () {
     todosArray = [];
     todosGenerator(todosArray);
-    localStorage.clear();
     localStorage.removeItem('todos');
     tasksCounter();
 }
 
+
+function addTask (event) {
+
+    let inputTaskValue = inputTask.value;
+
+    let newTodoObj = {
+        id: todosArray.length +1,
+        title: inputTaskValue
+    }
+
+    inputTask.value = '';
+
+    todosArray.push(newTodoObj);
+    setLocalStorage(todosArray);
+    todosGenerator(todosArray);
+    tasksCounter();
+
+    inputTask.focus();
+}
 window.addEventListener('load',getLocalStorage);
 clearBtn.addEventListener('click', clearTodos);
+inputTask.addEventListener('keydown', function(event){
+    if (event.code === "Enter") {
+        addTask();
+    }
+})
 
 
 
